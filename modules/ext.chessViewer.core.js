8983b990179ca9cb22e2a1364d9e0ff646e0e97c
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2026-09-03 04:18:30 +0000

    build: Updating npm dependencies
    * eslint-config-wikimedia: 0.31.0 → 0.32.4
    * svgo: 3.3.3 → 3.3.5
    * brace-expansion: 1.1.13, 2.1.4 → 1.1.18, 2.1.4
      * https://github.com/advisories/GHSA-3jxr-9vmj-r5cp
      * https://github.com/advisories/GHSA-mh99-v99m-4gvg
      * https://github.com/advisories/GHSA-rgw5-rvv9-x895
    * fast-uri: 3.1.2 → 3.1.7
      * https://github.com/advisories/GHSA-4c8g-83qw-93j6
      * https://github.com/advisories/GHSA-7p8r-x3mc-p8w7
      * https://github.com/advisories/GHSA-f65p-4m7j-42xc
      * https://github.com/advisories/GHSA-fph4-wmhf-6fwf
      * https://github.com/advisories/GHSA-jqff-g426-hqxp
      * https://github.com/advisories/GHSA-v2hh-gcrm-f6hx
    * js-yaml: 4.2.0 → 4.3.2
      * https://github.com/advisories/GHSA-52cp-r559-cp3m
      * https://github.com/advisories/GHSA-5p4m-2wfm-xmqj
    * nanoid: 3.3.11 → 3.3.18
      * https://github.com/advisories/GHSA-28wg-ghj8-5hjv
      * https://github.com/advisories/GHSA-2v37-7h3g-55p8
      * https://github.com/advisories/GHSA-xwg4-73v4-xw9w
    * postcss: 8.5.12 → 8.5.26
      * https://github.com/advisories/GHSA-fxqj-rqcc-2cmp
      * https://github.com/advisories/GHSA-r28c-9q8g-f849
    
    Additional changes:
    * Disabled Composer audit.block-insecure option (T416518)
    
    Change-Id: I2408b2645b222980d6d21f5009a6ccff2d21904e

--- modules/ext.chessViewer.core.js
+++ modules/ext.chessViewer.core.js
@@ -629,7 +629,6 @@
 				matches;
 			if ( move === 'O-O-O' ) {
 				me.announce( [
-					// eslint-disable-next-line mediawiki/msg-doc
 					mw.msg( colorMsg ),
 					mw.msg( 'chessbrowser-castling-queenside' )
 				].join( ' ' ) );
@@ -637,7 +636,6 @@
 			}
 			if ( move === 'O-O' ) {
 				me.announce( [
-					// eslint-disable-next-line mediawiki/msg-doc
 					mw.msg( colorMsg ),
 					mw.msg( 'chessbrowser-castling-kingside' )
 				].join( ' ' ) );
@@ -674,9 +672,7 @@
 			}
 
 			me.announce( [
-				// eslint-disable-next-line mediawiki/msg-doc
 				mw.msg( colorMsg ),
-				// eslint-disable-next-line mediawiki/msg-doc
 				mw.message( moveTypeMsg, piece, pieceSpecifier, position ),
 				promotion,
 				special
